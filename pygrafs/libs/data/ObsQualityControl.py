73aa348e225e8d7a9077ad221462a56c765d7d51
David John Gagne II <dgagne@ucar.edu>
2015-07-22 18:17:54 +0000

    Added bias checks to ObsQualityControl

--- pygrafs/libs/data/ObsQualityControl.py
+++ pygrafs/libs/data/ObsQualityControl.py
@@ -22,15 +22,19 @@ def main():
     print qc.data['haurwitz_kt'].values
     print qc.data['clearness_index'].values
     valid_counts = qc.invalid_count()
+    biases = qc.bias()
+    plt.figure(figsize=(8, 12))
     bmap = Basemap(projection="cyl", resolution="i", llcrnrlon=lon_bounds[0],
                    llcrnrlat=lat_bounds[0], urcrnrlon=lon_bounds[1], urcrnrlat=lat_bounds[1], fix_aspect=False)
     bmap.drawcountries()
     bmap.drawcoastlines()
     bmap.drawstates()
+    #plt.scatter(qc.station_data['lon'].values, qc.station_data["lat"].values, 20,
+    #            valid_counts["invalid"] / valid_counts["total"] * 100, vmin=0, vmax=50, edgecolors="", cmap="YlOrRd")
     plt.scatter(qc.station_data['lon'].values, qc.station_data["lat"].values, 20,
-                valid_counts["invalid"] / valid_counts["total"] * 100, vmin=0, vmax=50, edgecolors="", cmap="YlOrRd")
+                biases["kt_bias"].values, vmin=-0.5, vmax=0.5, cmap="RdBu_r")
     plt.grid()
-    plt.title("Valid Observation Percentage")
+    plt.title("Clearness Index Outside 0-1 Range Relative Frequency")
     plt.colorbar()
     plt.show()
     return
@@ -67,7 +71,7 @@ class ObsQualityControl(object):
         self.station_data.drop_duplicates(inplace=True)
 
     def clearness_index(self):
-        new_columns = ["zenith", "elevation", "azimuth", "apparent_zenith", "etr", "clearness_index",
+        new_columns = ["zenith", "elevation", "azimuth", "apparent_zenith", "etr", "etr_cos", "clearness_index",
                        "haurwitz_ghi", "haurwitz_kt"]
         for new_column in new_columns:
             self.data[new_column] = np.zeros((self.data.shape[0]))
@@ -79,11 +83,13 @@ class ObsQualityControl(object):
                            altitude=self.station_data.loc[station, "elev"])
             station_ix = self.data["station"] == station
             station_dates = pd.DatetimeIndex(self.data.loc[station_ix, "valid_date"])
-            station_dates = pd.DatetimeIndex(station_dates.shift(-1, freq="30Min"))
+            #station_dates = pd.DatetimeIndex(station_dates.shift(-1, freq="30Min"))
             sol_pos = get_solarposition(station_dates, loc)
-            sol_pos["etr"] = extraradiation(station_dates)
-            sol_pos["clearness_index"] = self.data.loc[station_ix, self.variable].values / \
-                (sol_pos["etr"] * np.cos(np.radians(sol_pos["apparent_zenith"])))
+            sol_pos["etr"] = extraradiation(station_dates, method="pyephem")
+            sol_pos["etr_cos"] = sol_pos["etr"] * np.cos(np.radians(sol_pos["apparent_zenith"]))
+            sol_pos.loc[sol_pos["apparent_zenith"] > 90, "etr_cos"] = 0
+            sol_pos["clearness_index"] = self.data.loc[station_ix, self.variable].values / sol_pos["etr_cos"]
+
             sol_pos["haurwitz_ghi"] = haurwitz(sol_pos["apparent_zenith"])
             sol_pos["haurwitz_kt"] = sol_pos["haurwitz_ghi"] / \
                 (sol_pos["etr"] * np.cos(np.radians(sol_pos["apparent_zenith"])))
@@ -93,7 +99,7 @@ class ObsQualityControl(object):
         valid_count = pd.DataFrame(data=np.zeros((self.station_data.shape[0], 3)),
                                    columns=["valid", "invalid", "total"])
         for s, station in enumerate(self.station_data.index):
-            si = self.data["station"] == station
+            si = (self.data["station"] == station) & (self.data["elevation"] > 0)
             valid_count.loc[s, "valid"] = np.count_nonzero((self.data.loc[si, 'clearness_index'] >= 0) &
                                               (self.data.loc[si, 'clearness_index'] <= 1))
             valid_count.loc[s, "invalid"] = np.count_nonzero(si) - valid_count.loc[s, "valid"]
@@ -101,6 +107,16 @@ class ObsQualityControl(object):
         valid_count.index = self.station_data.index
         return valid_count
 
+    def bias(self):
+        bias = pd.DataFrame(data=np.zeros((self.station_data.shape[0], 2)),
+                            index=self.station_data.index,
+                            columns=["kt_bias", "ghi_bias"])
+        for s, station in enumerate(self.station_data.index):
+            si = (self.data["station"] == station) & (self.data["elevation"] > 0)
+            bias.loc[station, "kt_bias"] = np.mean(self.data.loc[si, 'clearness_index']
+                                                   - self.data.loc[si, "haurwitz_kt"])
+            bias.loc[station, "ghi_bias"] = np.mean(self.data.loc[si, "av_dswrf_sfc"] - self.data.loc[si, "haurwitz_ghi"])
+        return bias
 
 if __name__ == "__main__":
     main()
