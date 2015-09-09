c6328116b3c2dad963e29fa4496ced41a060429a
David John Gagne <dgagne@ucar.edu>
2015-09-09 18:48:05 -0500

    Added function for parallel mesonet processing.

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 if __name__ == "__main__":
     setup(name="PyGRAFS",
-      version="0.1",
-      description="Gridded Atmospheric Forecast System",
-      author="David John Gagne",
-      author_email="dgagne@ucar.edu",
-      packages=["pygrafs", "pygrafs.libs", "pygrafs.apps","pygrafs.scripts", "pygrafs.scripts.plotting", 
-               "pygrafs.libs.data", "pygrafs.libs.util", "pygrafs.libs.model", "pygrafs.libs.evaluation"],
-      install_requires=["numpy>=1.8", "pandas>=0.15", "scipy", "matplotlib", "netCDF4", "scikit-learn>=0.15", 'pyproj'])
+          version="0.1",
+          description="Gridded Atmospheric Forecast System",
+          author="David John Gagne",
+          author_email="dgagne@ucar.edu",
+          packages=["pygrafs", "pygrafs.libs", "pygrafs.apps","pygrafs.scripts", "pygrafs.scripts.plotting",
+                    "pygrafs.libs.data", "pygrafs.libs.util", "pygrafs.libs.model", "pygrafs.libs.evaluation"],
+          install_requires=["numpy>=1.8", "pandas>=0.15", "scipy", "matplotlib",
+                            "netCDF4", "scikit-learn>=0.15", 'pyproj', "pvlib"])
