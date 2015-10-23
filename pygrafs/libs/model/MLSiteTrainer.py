36051d68689283613096946a11034bad8c94679e
David John Gagne II <dgagne@ucar.edu>
2015-10-23 03:31:32 +0000

    Addressed some issues with site forecasting

--- pygrafs/libs/model/MLSiteTrainer.py
+++ pygrafs/libs/model/MLSiteTrainer.py
@@ -2,6 +2,7 @@ from MLTrainer import MLTrainer
 from copy import deepcopy
 import cPickle
 import numpy as np
+import pandas as pd
 from gridding import linear_interpolation
 
 
@@ -54,19 +55,21 @@ class MLSiteTrainer(MLTrainer):
         shuffled_sites = np.random.permutation(all_sites)
         train_stations = shuffled_sites[:shuffled_sites.size / 2]
         test_stations = shuffled_sites[shuffled_sites.size/2:]
+        run_day_of_year = pd.DatetimeIndex(self.all_data["run_date"]).dayofyear
+        self.all_data["run_day_of_year"] = run_day_of_year
         train_data = self.all_data.loc[self.all_data['station'].isin(train_stations) &
-                                       (self.all_data['day_of_year'] % test_day_interval != 0)]
+                                       (run_day_of_year % test_day_interval != 0)]
         evaluation_data = self.all_data.loc[self.all_data['station'].isin(train_stations) &
-                                            (self.all_data['day_of_year'] % test_day_interval == 0)]
+                                            (run_day_of_year % test_day_interval == 0)]
         test_data = self.all_data.loc[self.all_data['station'].isin(test_stations) &
-                                      (self.all_data['day_of_year'] % test_day_interval == 0)]
+                                      (run_day_of_year % test_day_interval == 0)]
         predictions = test_data[pred_columns]
         site_predictions = evaluation_data[pred_columns]
         for m, model_obj in enumerate(model_objs):
             print model_names[m]
-            sites = np.unique(self.all_data[self.site_id_column].values)
             self.models[model_names[m]] = {}
-            for site_name in sites:
+            for site_name in train_stations:
+                print site_name
                 site_data = train_data.loc[train_data[self.site_id_column] == site_name]
                 self.models[model_names[m]][site_name] = deepcopy(model_obj)
                 self.models[model_names[m]][site_name].fit(site_data.loc[:, self.input_columns],
@@ -75,13 +78,13 @@ class MLSiteTrainer(MLTrainer):
                 site_predictions.loc[evaluation_data[self.site_id_column] == site_name,
                                      model_names[m]] = self.models[model_names[m]][site_name].predict(
                     eval_site_data.loc[:, self.input_columns])
-            for day in np.unique(evaluation_data["day_of_year"].values):
+            for day in np.unique(evaluation_data["run_day_of_year"].values):
+                print day
                 for hour in np.unique(evaluation_data["forecast_hour"].values):
-                    pred_rows = (test_data["day_of_year"] == day) & (test_data["forecast_hour"] == hour)
-                    predictions.loc[pred_rows, model_names[m]] = linear_interpolation(site_predictions,
-                                                                                      predictions, y_name, x_name)
+                    pred_rows = (test_data["run_day_of_year"] == day) & (test_data["forecast_hour"] == hour)
+                    eval_rows = (evaluation_data["run_day_of_year"] == day) & (evaluation_data["forecast_hour"] == hour)
+                    predictions.loc[pred_rows, model_names[m]] = linear_interpolation(site_predictions.loc[eval_rows, 
+                                                                                      [x_name, y_name, model_names[m]]],
+                                                                                      predictions.loc[pred_rows], y_name, x_name)
         return predictions
-
-
-
 
--- pygrafs/libs/model/gridding.py
+++ pygrafs/libs/model/gridding.py
@@ -14,12 +14,9 @@ def nearest_neighbor(predictions, grid_coordinates, y_name, x_name):
 
 
 def linear_interpolation(predictions, grid_coordinates, y_name, x_name):
+    print predictions.shape, grid_coordinates.shape, y_name, x_name
     lin = LinearNDInterpolator(predictions[[y_name, x_name]].values, predictions.ix[:, -1])
-    if len(grid_coordinates[x_name].shape) == 1:
-        y_grid, x_grid = np.meshgrid(grid_coordinates[y_name], grid_coordinates[x_name])
-        grid_predictions = lin(np.vstack((y_grid.ravel(), x_grid.ravel())).T)
-    else:
-        grid_predictions = lin(np.vstack((grid_coordinates[y_name].ravel(), grid_coordinates[x_name].ravel())).T)
+    grid_predictions = lin(np.vstack((grid_coordinates[y_name].ravel(), grid_coordinates[x_name].ravel())).T)
     return grid_predictions
 
 
